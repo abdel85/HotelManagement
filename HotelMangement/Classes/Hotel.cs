47228bd49cb85807e40a8c22a56c988f13965b50
Abdellatif <bouhellal.a@gmail.com>
2019-05-19 03:04:39 +0200

    HotelF

--- HotelMangement/Classes/Hotel.cs
+++ HotelMangement/Classes/Hotel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,53 @@ namespace HotelMangement.Classes
 {
     public class Hotel
     {
+        private int id;
+        private string name;
+        private int roomNb;
+        private SqlCommand command;
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public int RoomNb { get => roomNb; set => roomNb = value; }
+        public SqlCommand Command { get => command; set => command = value; }
+
+        public Hotel()
+        {
+            
+        }
+
+        public Hotel(string name)
+        {
+            command = new SqlCommand("SELECT * FORM Hotel WHERE Name = @name", Connection.Instance);
+            command.Parameters.Add(new SqlParameter("@n", name));
+            Connection.Instance.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            if (reader.Read())
+            {
+                Id = reader.GetInt32(0);
+                Name = reader.GetString(1);
+                RoomNb = reader.GetInt32(2);
+
+            }
+            reader.Close();
+            command.Dispose();
+            Connection.Instance.Close();
+ 
+        }
+
+        public bool Save()
+        {
+            bool result = false;
+            command = new SqlCommand("INSERT INTO Hotel(Name,RoomNb) OUTPUT INSETED.ID VALUE(@name,@roomNb)",Connection.Instance);
+            command.Parameters.Add(new SqlParameter("@name", Name));
+            command.Parameters.Add(new SqlParameter("@roomNb", RoomNb));
+            Connection.Instance.Open();
+            Id = (int) command.ExecuteScalar();
+            command.Dispose();
+            Connection.Instance.Close();
+            return result;
+        }
     }
+
+    
 }
--- HotelMangement/Program.cs
+++ HotelMangement/Program.cs
@@ -1,15 +1,112 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using HotelMangement.Classes;
 
 namespace HotelMangement
 {
     class Program
     {
+        static Hotel hotel;
         static void Main(string[] args)
         {
+            Init();
+            Console.ReadLine();
+        }
+
+        static void Menu()
+        {
+            int choice = 4;
+            do
+            {
+                Console.WriteLine("Choose a Number to select an option");
+                Console.WriteLine("1-Costumer's List");
+                Console.WriteLine("2-Booking List");
+                Console.WriteLine("3-Add Booking");
+                Console.WriteLine("0-to Exit");
+                try
+                {
+                    choice = Convert.ToInt32(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            GetCostumer();
+                            break;
+                        case 2:
+                            GetBooking();
+                            break;
+                        case 3:
+                            AddBooking();
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                
+            }
+            while (choice != 0);
+        }
+
+        static void Init()
+        {
+            int choice = 3;
+            do
+            {
+                Console.WriteLine("1-New Hotel");
+                Console.WriteLine("2-Choose Hotel");
+                Console.WriteLine("0-Exit");
+
+                try
+                {
+                    choice = Convert.ToInt32(Console.ReadLine());
+
+                    switch (choice)
+                    {
+                        case 1:
+                            NewHotel();
+                            break;
+                        case 2:
+                            ChooseHotel();
+                            break;
+                        case 0:
+                            Environment.Exit(0);
+                            break;
+
+                        
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                  
+                }
+
+            }
+            while (choice != 3);
+        }
+
+        static void NewHotel()
+        {
+            Console.WriteLine("Hotel Name:");
+            string input = Console.ReadLine();
+            int roomNb;
+            do
+            {
+                Console.WriteLine("Rooms Number :");
+                Int32.TryParse(Console.ReadLine(), out roomNb);
+            } while (roomNb == 0);
+
+
+        }
+
+        static void ChooseHotel()
+        {
+
         }
     }
 }
