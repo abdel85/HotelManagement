04bf2074a29f51e69fd71c529f964192a9602617
Abdellatif <bouhellal.a@gmail.com>
2019-05-19 02:05:13 +0200

    InvoiceF

--- HotelMangement/Classes/Invoice.cs
+++ HotelMangement/Classes/Invoice.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,7 +8,36 @@ using System.Threading.Tasks;
 namespace HotelMangement.Classes
 {
    public class Invoice
-    {
+   {
+       private int customerId;
+       private int id;
+       private decimal price;
+       private InvoiceStatus status;
+       private static SqlCommand command;
+
+        public int CustomerId { get => customerId; set => customerId = value; }
+        public int Id { get => id; set => id = value; }
+        public decimal Price { get => price; set => price = value; }
+        public InvoiceStatus Status { get => status; set => status = value; }
+
+        public static SqlCommand Command { get => command; set => command = value; }
+
+        public bool Save()
+        {
+            bool result = false;
+            command = new SqlCommand("INSERT INTO invoice (customerId, price, satuts) OUTPUT INSERTED.ID VALUES (@c,@pc@s)", Connection.Instance);
+            command.Parameters.Add(new SqlParameter("@", customerId));
+            command.Parameters.Add(new SqlParameter("@", price));
+            command.Parameters.Add(new SqlParameter("@", Status));
+
+            Connection.Instance.Open();
+            Id = (int)command.ExecuteScalar();
+            command.Dispose();
+            Connection.Instance.Close();
+            result = (id > 0);
+            return result;
+        }
+
 
         public enum InvoiceStatus
         {
